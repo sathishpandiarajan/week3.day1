738b692ae8dccb0b8b7ef97e25b7997b1b5488fc
sathishpandiarajan <96656739+sathishpandiarajan@users.noreply.github.com>
2022-01-09 00:12:30 +0530

    Add files via upload

---
+++ College.java
@@ -0,0 +1,22 @@
+package org.college;
+
+public class College 
+{
+	public void collegeName() 
+	{
+		System.out.println("College Name is - Test Leaf");
+	}
+	public void collegeCode() 
+	{
+		System.out.println("College Code is - TL");
+	}
+	public void collegeRank() 
+	{
+		System.out.println("College Rank is - O+");
+	}
+	public static void main(String[] args) 
+	{
+		
+	}
+
+}
