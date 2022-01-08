bbe8e01102480da282643213c2535fa70f37ae6f
sathishpandiarajan <96656739+sathishpandiarajan@users.noreply.github.com>
2022-01-09 00:13:16 +0530

    Add files via upload

---
+++ Department.java
@@ -0,0 +1,17 @@
+package org.department;
+
+import org.college.College;
+
+public class Department extends College
+{
+	public void deptName() 
+	{
+		System.out.println("Department Name is Selenium Testing");
+	}
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
